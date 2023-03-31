0f64f7f0b6b8f1b4fac973df6cb22712204f0588
sabaly_mac <tedoldhead@gmail.com>
2023-03-31 21:26:38 +0800

    add xlabel's datetime

--- highs_lows.py
+++ highs_lows.py
@@ -1,16 +1,29 @@
 import csv
 from matplotlib import pyplot as plt
+from datetime import datetime
 
-filename = 'sitka_weather_07-2014.csv'
+filename = 'sitka_weather_2014.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
     for index, column_header in enumerate(header_row):
         print(index, column_header)
-    #讀取每日最高溫    
-    highs = []
+    #讀取每日最高溫，及日期    
+    dates, highs = [], []
     for row in reader:
+        current_date = datetime.strptime(row[0], "%Y-%m-%d")
+        dates.append(current_date)
         high = int(row[1])
         highs.append(high)
         
 #根據數據繪製圖形    
+fig = plt.figure(dpi=128, figsize=(10, 6))
+plt.plot(dates, highs, c='red')
+#設置圖形的格式
+plt.title("Daily high temperatures, July 2014", fontsize=24)
+plt.xlabel('', fontsize=16)
+fig.autofmt_xdate()
+plt.ylabel("Temperature (F)", fontsize=16)
+#設計科度標記大小
+plt.tick_params(axis='both', which='major', labelsize=16, color='r')
+plt.show()
